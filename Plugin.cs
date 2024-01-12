795863d0848b399974e6d33a3cb012879e121a3c
mindofbrandon <zunigabrandon12@gmail.com>
2024-01-11 20:25:30 -0500

    initial setup

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace PunchingMod
 {
-    public class PunchingMod
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class PunchingMod : BaseUnityPlugin
     {
+        private const string modGUID = "StretchySpaghetti.PunchingMod";
+        private const string modName = "Punching Mod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static PunchingMod Instance;
+
+        internal ManualLogSource manualLogSource;
+
+
+        // Entry point 
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            manualLogSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            manualLogSource.LogInfo("Starting up PunchingMod");
+
+            harmony.PatchAll(); // patches mod
+
+
+        }
+
     }
 }
---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# Punching Mod
+
+## Tools used
+- Visual Studio
+- .Net7 Framework
+- ILSpy 8.2
+- Bepinex 5.4.22
+
+
+
+### Special Thanks
+- MrMiinxx for his tutorials
+
+
